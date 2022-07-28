ec8a73c8e615000be0b56bf89c4c524f0bed9321
Chloe1997 <42937407+Chloe1997@users.noreply.github.com>
2022-07-28 16:16:04 +0800

    Add files via upload

--- connectMysql.php
+++ connectMysql.php
@@ -2,8 +2,8 @@
     //資料庫主機設定
     $db_host = "localhost";
     $db_table = "animation";
-    $db_username = "root";
-    $db_password = "86094195";
+    $db_username = "your_username";
+    $db_password = "your_password";
     //設定資料庫連線
     $db_link = @mysqli_connect($db_host,$db_username,$db_password)
     or die("Fail to connect with Mysql");
